5c13f7ab635179254e13e398920534d4233f55f4
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-09-13 22:00:49 -0600

    feat: add healthcheck endpoint

---
+++ internal/platform/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gohex.com/m/internal/platform/server/handler/health"
+)
+
+type Server struct {
+	httpAddr string
+	engine   *gin.Engine
+}
+
+func New(host string, port uint) Server {
+	srv := Server{
+		engine: gin.New(),
+		httpAddr: fmt.Sprintf("%s:%d", host, port),
+	}
+
+	srv.registerRoutes()
+	return srv
+}
+
+func (s *Server) Run() error {
+	log.Println("Server running in ", s.httpAddr)
+	return s.engine.Run(s.httpAddr)
+}
+
+func (s *Server) registerRoutes() {
+	s.engine.GET("/health", health.CheckHandler())
+}
